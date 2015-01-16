c0064b789b210d5c947107cfa26d08050007e14b
Shad Holland <shad.holland@gmail.com>
2015-01-16 17:30:13 -0600

    Updated routing so have a default route and also allow for a hash to trigger a route on initial load

--- js/shad.js
+++ js/shad.js
@@ -2,24 +2,47 @@ var app = new Object();
 
 app.init = function() {
 	/** Init Routes **/
+	this.loadRoute();
 	this.routeListener();
+
 }
 
 app.getTemplate = function( path, element ) {
+	//Get view template and update view based on element passed in
 	$.ajax( { url: path } ).done( function( response ) {
 		$( element ).html( response );
 	});	
 }
 
+/** Routing **/
+
 app.routes = [];
 
+app.loadRoute = function() {
+	if( window.location.hash ) {
+		app.findRoute();
+	}
+	else {
+    	var routeObj = app.routes[ 0 ];
+    	app.getTemplate( routeObj.template, '.content' );
+	}
+}
+
 app.routeListener = function() {
+	//Check when hash changes and update view
 	window.onhashchange = function () {
-		var route = window.location.hash.split('#')[1];
-	    for( x in app.routes ) {
-	    	if( app.routes[ x ].route === route ) {
-	    		app.getTemplate( app.routes[ x ].template, '.content' );
-	    	}
-	    }
+		app.findRoute();
 	}	
 }
+
+app.findRoute = function() {
+	var routeName = window.location.hash.split('#')[1];
+    for( x in app.routes ) {
+    	var routeObj = app.routes[ x ];
+    	if( routeObj.route === routeName ) {
+    		app.getTemplate( routeObj.template, '.content' );
+    	}
+    }	
+}
+
+/** End Routing **/
